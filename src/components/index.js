5513c7a208f5119967ab68465457bd349ed9686e
mbj36 <mohitbajo36@gmail.com>
2018-05-29 23:01:02 +0530

    Added different strategy

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,17 @@
 import Facebook from './Facebook';
 import Twitter from './Twitter';
+import Telegram from './Telegram';
+import WhatsApp from './Whatsapp';
+import LinkedIn from './LinkedIn';
+import Quora from './Quora';
 
 const components = {
   Facebook,
-  Twitter
+  Twitter,
+  LinkedIn,
+  WhatsApp,
+  Telegram,
+  Quora
 };
 
 export default components;
