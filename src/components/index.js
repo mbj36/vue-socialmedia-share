975376ce5370b322123f0b3a338e77e58bbbb501
mbj36 <mohitbajo36@gmail.com>
2018-05-30 21:14:31 +0530

    0.8.0

--- src/components/index.js
+++ src/components/index.js
@@ -1,23 +1,9 @@
-import Facebook from './Facebook';
-import Twitter from './Twitter';
-import Telegram from './Telegram';
-import WhatsApp from './Whatsapp';
-import LinkedIn from './LinkedIn';
-import Pinterest from './Pinterest';
-import Reddit from './Reddit';
-import Email from './Email';
-import Google from './Google';
-
-const components = {
-  Facebook,
-  Twitter,
-  Pinterest,
-  Email,
-  Google,
-  LinkedIn,
-  WhatsApp,
-  Telegram,
-  Reddit
-};
-
-export default components;
+export * from './Facebook';
+export * from './Twitter';
+export * from './Telegram';
+export * from './Whatsapp';
+export * from './LinkedIn';
+export * from './Pinterest';
+export * from './Reddit';
+export * from './Email';
+export * from './Google';
