99abff3f57c1a9d703f0129592fe0852dd08ba16
mbj36 <mohitbajo36@gmail.com>
2018-05-30 19:50:23 +0530

    Bugs

--- src/components/index.js
+++ src/components/index.js
@@ -4,14 +4,20 @@ import Telegram from './Telegram';
 import WhatsApp from './Whatsapp';
 import LinkedIn from './LinkedIn';
 import Pinterest from './Pinterest';
+import Reddit from './Reddit';
+import Email from './Email';
+import Google from './Google';
 
 const components = {
   Facebook,
   Twitter,
   Pinterest,
+  Email,
+  Google,
   LinkedIn,
   WhatsApp,
-  Telegram
+  Telegram,
+  Reddit
 };
 
 export default components;
